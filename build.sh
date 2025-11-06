d82f0d5e2b7f5ed336825d22b717001dabc3578e
vaibhav Patil <vp14541610@gmail.com>
2025-11-06 14:34:16 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,1 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
 
-# ✅ Force correct Python version
-echo "Forcing Python 3.11 installation..."
-pyenv install -s 3.11.9
-pyenv global 3.11.9
-python -m pip install --upgrade pip setuptools wheel
-
-# ✅ Install dependencies and migrate
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
-python manage.py migrate
